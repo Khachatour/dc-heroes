364610d498e81f177b4581f5c2c4050af3a181f9
Khachatour <loledgamer96@hotmail.com>
2017-12-15 09:55:58 +0400

    app: inital commit eslint configured

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import RootNavigation from './navigation/RootNavigation';
-import { Examples } from '@shoutem/ui'
+import React from 'react'
+import { Platform, StatusBar, StyleSheet, View } from 'react-native'
+import { AppLoading, Asset, Font } from 'expo'
+import { Ionicons } from '@expo/vector-icons'
+import { Heading, Image, NavigationBar, Title, Icon } from '@shoutem/ui'
 
-
-console.disableYellowBox = true;
+console.disableYellowBox = true
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
     fontsAreLoaded: false
-  };
+  }
 
-  async componentWillMount() {
+  async componentWillMount () {
     await Font.loadAsync({
       'Rubik-Black': require('./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf'),
       'Rubik-BlackItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf'),
@@ -26,35 +24,40 @@ export default class App extends React.Component {
       'Rubik-Medium': require('./node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf'),
       'Rubik-MediumItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf'),
       'Rubik-Regular': require('./node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf'),
-      'rubicon-icon-font': require('./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf'),
-    });
+      'rubicon-icon-font': require('./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf')
+    })
 
-    this.setState({fontsAreLoaded: true});
+    this.setState({ fontsAreLoaded: true })
   }
 
-
-  render() {
+  render () {
     if (!this.state.fontsAreLoaded) {
-      return <AppLoading />;
+      return <AppLoading />
     }
-
     return (
-      <View style={{flex: 1}}>
-        <Examples />
-        <StatusBar barStyle="default" hidden={false} />
+      <View style={styles.container}>
+        <View style={styles.box} />
       </View>
-    );
+    )
   }
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3b4565'
+    // ,#302b63, #24243e)'
   },
-  statusBarUnderlay: {
-    height: 24,
-    backgroundColor: 'rgba(0,0,0,0.2)',
-  },
-});
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#ce7eca',
+    shadowColor: 'rgba(0,0,0,0.30)',
+    shadowOffset: { width: 5, height: 10 },
+    shadowOpacity: 1,
+    shadowRadius: 2,
+    borderRadius: 20
+  }
+})
